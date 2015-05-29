b590c11610d8374b12c013ee95fed9feaa703c97
Michael Prasuhn <mike@mikeyp.net>
2015-05-28 23:45:06 -0700

    Work in progress on conditional checks

--- src/Form/GoogleTagSettingsForm.php
+++ src/Form/GoogleTagSettingsForm.php
@@ -15,7 +15,6 @@ use Drupal\Core\Form\FormStateInterface;
  * @package Drupal\google_tag\Form
  */
 class GoogleTagSettingsForm extends ConfigFormBase {
-
   /**
    * {@inheritdoc}
    */
@@ -52,11 +51,11 @@ class GoogleTagSettingsForm extends ConfigFormBase {
       '#group' => 'settings',
     ];
 
-    $form['general']['google_tag_container_id'] = [
+    $form['general']['container_id'] = [
       '#type' => 'textfield',
       '#title' => $this->t('Container ID'),
       '#description' => $this->t('The ID assigned by Google Tag Manager (GTM) for this website container. To get a container ID, <a href="http://www.google.com/tagmanager/web/">sign up for GTM</a> and create a container for your website.'),
-      '#default_value' => '', //variable_get('google_tag_container_id', ''),
+      '#default_value' => $config->get('container_id'),
       '#attributes' => ['placeholder' => ['GTM-xxxxxx']],
       '#size' => 11,
       '#maxlength' => 15,
@@ -75,20 +74,20 @@ class GoogleTagSettingsForm extends ConfigFormBase {
       '#description' => $description,
     ];
 
-    $form['paths']['google_tag_path_toggle'] = [
+    $form['paths']['path_toggle'] = [
       '#type' => 'radios',
       '#title' => $this->t('Add snippet on specific paths'),
       '#options' => [
-        $this->t('All paths except the listed paths'),
-        $this->t('Only the listed paths'),
+        GOOGLE_TAG_DEFAULT_INCLUDE => $this->t('All paths except the listed paths'),
+        GOOGLE_TAG_DEFAULT_EXCLUDE => $this->t('Only the listed paths'),
       ],
-      '#default_value' => 0, //variable_get('google_tag_path_toggle', 0),
+      '#default_value' => $config->get('path_toggle'),
     ];
-    $form['paths']['google_tag_path_list'] = [
+    $form['paths']['path_list'] = [
       '#type' => 'textarea',
       '#title' => $this->t('Listed paths'),
       '#description' => $this->t('Enter one relative path per line using the "*" character as a wildcard. Example paths are: "%blog" for the blog page, "%blog-wildcard" for each individual blog, and "%front" for the front page.', ['%blog' => 'blog', '%blog-wildcard' => 'blog/*', '%front' => '<front>']),
-      '#default_value' => '', //variable_get('google_tag_path_list', GOOGLETAGMANAGER_PATHS),
+      '#default_value' => $config->get('path_list'),
       '#rows' => 10,
     ];
 
@@ -100,24 +99,24 @@ class GoogleTagSettingsForm extends ConfigFormBase {
       '#group' => 'settings',
     ];
 
-    $form['roles']['google_tag_role_toggle'] = [
+    $form['roles']['role_toggle'] = [
       '#type' => 'radios',
       '#title' => t('Add snippet for specific roles'),
       '#options' => [
-        t('All roles except the selected roles'),
-        t('Only the selected roles'),
+        GOOGLE_TAG_DEFAULT_INCLUDE => $this->t('All roles except the selected roles'),
+        GOOGLE_TAG_DEFAULT_EXCLUDE => $this->t('Only the selected roles'),
       ],
-      '#default_value' => 0, //variable_get('google_tag_role_toggle', 0),
+      '#default_value' => $config->get('role_toggle'),
     ];
 
     $user_roles = array_map(function($role) {
       return $role->label();
     }, user_roles());
 
-    $form['roles']['google_tag_role_list'] = [
+    $form['roles']['role_list'] = [
       '#type' => 'checkboxes',
       '#title' => t('Selected roles'),
-      '#default_value' => [], //variable_get('google_tag_role_list', array()),
+      '#default_value' => $config->get('role_list'),
       '#options' => $user_roles,
     ];
 
@@ -132,18 +131,18 @@ class GoogleTagSettingsForm extends ConfigFormBase {
     ];
 
 
-    $form['statuses']['google_tag_status_toggle'] = [
+    $form['statuses']['status_toggle'] = [
       '#type' => 'checkbox',
       '#title' => $this->t('Override path condition for listed response statuses'),
       '#description' => $this->t('If checked, then the path condition will be ingored for a listed page response status.'),
-      '#default_value' => 0, //variable_get('google_tag_status_toggle', 0),
+      '#default_value' => $config->get('status_toggle'),
     ];
 
-    $form['statuses']['google_tag_status_list'] = [
+    $form['statuses']['status_list'] = [
       '#type' => 'textarea',
       '#title' => $this->t('Listed statuses'),
       '#description' => $list_description,
-      '#default_value' => '', //variable_get('google_tag_status_list', GOOGLETAGMANAGER_STATUSES),
+      '#default_value' => $config->get('status_list'),
       '#rows' => 5,
     ];
 
@@ -154,40 +153,52 @@ class GoogleTagSettingsForm extends ConfigFormBase {
       '#group' => 'settings',
     ];
 
-    $form['advanced']['google_tag_compact_tag'] = [
+    $form['advanced']['compact_tag'] = [
       '#type' => 'checkbox',
       '#title' => $this->t('Compact the JavaScript snippet'),
       '#description' => $this->t('If checked, then the JavaScript snippet will be compacted to remove unnecessary whitespace. This is <strong>recommended on production sites</strong>. Leave unchecked to output a snippet that can be examined using a JavaScript debugger in the browser.'),
-      '#default_value' => 1, //variable_get('google_tag_compact_tag', 1),
+      '#default_value' => $config->get('compact_tag'),
     ];
-
-
 
     return parent::buildForm($form, $form_state);
   }
 
+  /**
+   * {@inheritdoc}
+   */
   public function validateForm(array &$form, FormStateInterface $form_state) {
     // Trim the text values.
-    $values['google_tag_container_id'] = trim($values['google_tag_container_id']);
-    $values['google_tag_path_list'] = trim($values['google_tag_path_list']);
-    $values['google_tag_status_list'] = trim($values['google_tag_status_list']);
+    $container_id = trim($form_state->getValue('container_id'));
+    $form_state->setValue('path_list', trim($form_state->getValue('path_list')));
+    $form_state->setValue('status_list', trim($form_state->getValue('status_list')));
 
     // Replace all types of dashes (n-dash, m-dash, minus) with a normal dash.
-    $values['google_tag_container_id'] = str_replace(['–', '—', '−'], '-', $values['google_tag_container_id']);
+    $container_id = str_replace(['–', '—', '−'], '-', $container_id);
 
-    if (!preg_match('/^GTM-\w{4,}$/', $values['google_tag_container_id'])) {
+    if (!preg_match('/^GTM-\w{4,}$/', $container_id)) {
       // @todo Is there a more specific regular expression that applies?
       // @todo Is there a way to "test the connection" to determine a valid ID for
       // a container? It may be valid but not the correct one for the website.
-      form_set_error('google_tag_container_id', t('A valid container ID is case sensitive and formatted like GTM-xxxxxx.'));
+      $form_state->setError($form['general']['container_id'], $this->t('A valid container ID is case sensitive and formatted like GTM-xxxxxx.'));
     }
-
 
     parent::validateForm($form, $form_state);
   }
 
+  /**
+   * {@inheritdoc}
+   */
   public function submitForm(array &$form, FormStateInterface $form_state) {
-
+    $this->config('google_tag.settings')
+      ->set('container_id', $form_state->getValue('container_id'))
+      ->set('path_toggle', $form_state->getValue('path_toggle'))
+      ->set('path_list', $form_state->getValue('path_list'))
+      ->set('role_toggle', $form_state->getValue('role_toggle'))
+      ->set('role_list', $form_state->getValue('role_list'))
+      ->set('status_toggle', $form_state->getValue('status_toggle'))
+      ->set('status_list', $form_state->getValue('status_list'))
+      ->set('compact_tag', $form_state->getValue('compact_tag'))
+      ->save();
 
     parent::submitForm($form, $form_state);
   }
